eb7d5b3694d4098839b6566324c9c36e1b93b2d3
Lucas Nunes <lucasnunesti01@gmail.com>
2026-04-14 06:23:40 -0300

    Adicionando classe Moto com cálculo de diária

---
+++ ProjetoLocadora/src/Moto.java
@@ -0,0 +1,11 @@
+class Moto extends Veiculo {
+
+    public Moto(String marca, String modelo, int ano, String placa, double precoBase) {
+        super(marca, modelo, ano, placa, precoBase);
+    }
+
+    @Override
+    public double calcularDiaria() {
+        return getPrecoBase() * 0.9; // 10% mais barato que o base
+    }
+}
